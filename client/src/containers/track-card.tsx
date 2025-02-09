f90cb6efdf183c5cacdf78bf4b8d1a8bafad29fa
qadeerassan <45184523+qadeerassan@users.noreply.github.com>
2025-02-08 23:41:58 -0500

    graph developer certified

--- client/src/containers/track-card.tsx
+++ client/src/containers/track-card.tsx
@@ -4,16 +4,50 @@ import { colors, mq } from "../styles";
 import { humanReadableTimeFromSeconds } from "../utils/helpers";
 import { Link } from "react-router-dom";
 import type { Track } from "../__generated__/graphql";
-
+import { useMutation } from "@apollo/client";
+import { gql } from "../__generated__";
 /**
  * Track Card component renders basic info in a card format
  * for each track populating the tracks grid homepage.
  */
+
+/**
+* Mutation to increment a track's number of views
+*/
+const INCREMENT_TRACK_VIEWS = gql(`
+  mutation IncrementTrackViews($incrementTrackViewsId: ID!) {
+    incrementTrackViews(id: $incrementTrackViewsId) {
+      code
+      success
+      message
+      track {
+        id
+        numberOfViews
+      }
+    }
+  }
+`);
+
 const TrackCard: React.FC<{ track: Omit<Track, "modules"> }> = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track;
+  
+  useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: { incrementTrackViewsId: id },
+  });
+
+  const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: { incrementTrackViewsId: id },
+      // to observe what the mutation response returns
+    onCompleted: (data) => {
+    console.log(data);
+  },
+  });
 
   return (
-    <CardContainer to={`/track/${id}`}>
+    <CardContainer 
+    to={`/track/${id}`}
+    onClick={() => incrementTrackViews()}
+    >
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail || ""} alt={title} />
